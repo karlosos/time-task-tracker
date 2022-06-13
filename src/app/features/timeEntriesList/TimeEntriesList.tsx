f6aa0df582f1cea19fc3fad2d549f6a001430a3e
Karol Dzialowski <karlososhd@gmail.com>
2022-06-13 18:28:53 +0200

    ✨ Add combined time for day

--- src/app/features/timeEntriesList/TimeEntriesList.tsx
+++ src/app/features/timeEntriesList/TimeEntriesList.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from "../../hooks";
 import {
   selectCombinedTimeEntries,
 } from "../../store/timeEntries";
+import { formatElapsedTime } from "../../utils";
 import { CombinedTimeEntryRow } from "./CombinedTimeEntryRow";
 import { DateLabel, TimeEntriesDateSublist, TimeEntriesListStyled } from "./TimeEntriesList.style";
 
@@ -12,9 +13,10 @@ export const TimeEntriesList = () => {
       return Object.entries(combinedTimeEntries)
         .sort((a, b) => (a[0] > b[0] ? -1 : 1))
         .map(([date, combinedTimeEntriesPerDate]: any[]) => {
+          const elapsedTimePerDay = combinedTimeEntriesPerDate.reduce((acc: number, combinedTimeEntries: any) => acc + combinedTimeEntries.elapsedTime, 0)
           return (
             <TimeEntriesDateSublist key={date}>
-              <DateLabel>{date}</DateLabel>
+              <DateLabel>{date} {formatElapsedTime(elapsedTimePerDay)}</DateLabel>
               {combinedTimeEntriesPerDate.map((combinedTimeEntries: any) => (
                 <CombinedTimeEntryRow
                   combinedTimeEntry={combinedTimeEntries}
