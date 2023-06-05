a98a888eea8807495abc4209519ad73153c09240
Karol Dzialowski <karlososhd@gmail.com>
2023-06-05 20:41:03 +0200

    💄 Improve HoverCard delay times

--- src/app/ui/HoverCard.tsx
+++ src/app/ui/HoverCard.tsx
@@ -2,14 +2,18 @@ import * as React from "react";
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 import { cn } from "../lib/utils";
 
-const HoverCard = HoverCardPrimitive.Root;
+const HoverCard: React.FC<HoverCardPrimitive.HoverCardProps> = (props) => (
+  <HoverCardPrimitive.Root {...props} openDelay={300} closeDelay={0}>
+    {props.children}
+  </HoverCardPrimitive.Root>
+);
 
 const HoverCardTrigger = HoverCardPrimitive.Trigger;
 
 const HoverCardContent = React.forwardRef<
   React.ElementRef<typeof HoverCardPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+>(({ className, align = "start", sideOffset = 0, ...props }, ref) => (
   <HoverCardPrimitive.Content
     ref={ref}
     align={align}
