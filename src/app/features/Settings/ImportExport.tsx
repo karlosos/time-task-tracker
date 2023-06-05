9cfa680b3dc032b1f0c7a007ae71dfbd9d840309
Karol Dzialowski <karlososhd@gmail.com>
2023-06-05 20:01:16 +0200

    ✨ Download app data

--- src/app/features/Settings/ImportExport.tsx
+++ src/app/features/Settings/ImportExport.tsx
@@ -4,13 +4,16 @@ import {
   Download,
   FileDown,
   FileUp,
-  HelpCircle,
   Radiation,
 } from "lucide-react";
+import { useAppDispatch } from "../../hooks";
 import { Button } from "../../ui/Button";
 import { Support } from "../../ui/Support";
+import { downloadAppData } from "./slice";
 
 export const ImportExport = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <div>
@@ -47,9 +50,9 @@ export const ImportExport = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
           <span className="sr-only">Info</span>
@@ -86,7 +89,7 @@ export const ImportExport = () => {
             Exported data can be later imported or read manually. Data is
             exported in JSON format.
           </div>
-          <Button variant="outline">
+          <Button variant="outline" onClick={() => dispatch(downloadAppData())}>
             <FileDown className="mr-2 h-4 w-4" />
             Download
           </Button>
