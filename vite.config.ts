28dbc0b266004f2429d7a9b0c15c6ebaa0bbe2df
Karol Dzialowski <karlososhd@gmail.com>
2025-01-03 19:48:34 +0100

    ⬆️  Use vite instead of cra

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Optional: Adjust if you want to use `@` as an alias for `src`
+    },
+  },
+});
