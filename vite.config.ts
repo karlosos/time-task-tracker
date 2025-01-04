f38fa5497d2ec1f067483e86ec347d45eb4a443a
Karol Dzialowski <karlososhd@gmail.com>
2025-01-04 20:34:24 +0100

    🎨 Fix code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Optional: Adjust if you want to use `@` as an alias for `src`
-    },
-  },
 });
